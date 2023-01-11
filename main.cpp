4da86c2807a678f5ff1e48ae6c215e467ae8047c
raven <1772133019@qq.com>
2023-01-11 04:14:23 +0000

    1214

--- main.cpp
+++ main.cpp
@@ -4,90 +4,33 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "myGame.h"
+// #include <SFML/Graghcs.hpp>
 
 #define log(x) std::cout << "\n\t" << x << std::endl
 
-using namespace std;
 
-void gameLoading(func& func, int systemNum){
-    // 啥呗进度条
-    func.clear(systemNum);
-    for(int i=0; i<1; i++)
-    {
-        func.clear(systemNum);
-        log("正在加载.");
-        sleep(1);
-        func.clear(systemNum);
-        log("正在加载..");
-        sleep(1);
-        func.clear(systemNum);
-        // log("正在加载...");
-        // sleep(1);
-    }
-    sleep(1);
-}
-
-void gameStart(func& func, text& gameText, int systemNum){
-    func.clear(systemNum);
-    gameText.printHello();
-    func.press_any_key_to_continue();
-}
 
 
 int main() {
     // -- 初始化 --
     // system("iconv -f utf-8 -t gbk main.exe > main.gbk.exe"); // 不知道能不能行 // 不能行
-    text gameText;
+    text text;
     func func;
     character player(10,2,2);
     character enemy(10,3,0);
+    battle battle;
+
     // -- 判断系统 --
-    // log(func.whichSystem());
     func.displayWhichSystem(func.whichSystem());
-    int systemNum = func.whichSystem();
     func.press_any_key_to_continue();
 
-    gameLoading(func, systemNum);
-    gameStart(func, gameText, systemNum);
-    func.clear(systemNum);
+    func.gameLoading();
+    func.gameStart(text);
+    func.clear(func.systemNum);
 
-    while (player.isAlive() && enemy.isAlive()) {
-        cout << "your hp is:" << player.getHP() << endl;
-        cout << "your enemy's hp is:" << enemy.getHP() << endl;
-        cout << "\nwhat's your choice!!!\n\t1.attack\t2.defence\n" << endl;
-
-        int playerChoice;
-        cin >> playerChoice;
-
-        switch (playerChoice) {
-        case 1:
-            log("you are attacking your enemy!!");
-            // player.attack(enemy);
-            sleep(1);
-            cout << "你对敌方造成了" << player.attack(enemy) << "点伤害" << endl;
-            sleep(1);
-            func.clear(systemNum);
-            break;
-        case 2:
-            log("your enemy is going to attack you! DEFENCE!");
-            sleep(1);
-            player.increaseDefForOneRound();
-            cout << "你的防御力提高到了" << player.getDefence() + 5 << "点" << endl;
-            // cout << "你的防御力提高到了" << player.getDefence() + 5 << "点" << endl;
-            sleep(1);
-            cout << "敌方实际对你造成了" << enemy.attack(player) << "点伤害!" << endl;
-            player.decreaseDefForOneRound();
-            sleep(1);
-            func.clear(systemNum);
-            break;
-        default:
-            log("输数字，ok？");
-            sleep(1);
-            func.clear(systemNum);
-            break;
-        }
-    }
-
+    // 战斗环节
+    battle.battleField(player, enemy, func);
+    
     // -- 游戏结束 --
     if(player.isAlive()){
         log("恭喜你获胜了！");
--- myGame.h
+++ myGame.h
@@ -3,28 +3,44 @@
 
 #include <iostream>
 #include <ctime>
-#include <unistd.h>
+#include <type_traits>
 #include <stdlib.h>
 #include <stdio.h>
 #include <random>
 
-#define log(x) std::cout << "\n\t" << x << std::endl
+#ifdef _WIN32
+    #include <Windows.h>
+#elif __linux__
+    #include <sys/ioctl.h>
+    #include <unistd.h>
+#endif
 
-using namespace std;
+#define log(x) std::cout << "\n\t" << x << std::endl
+using std::cout;
+using std::endl;
+using std::cin;
+
 
 class text{
     public:
-        int printHello();
+        int const printHello();
         int printHintForUnreadableCode();
+        void static alignCenterLinux(std::string text);
 };
 
-class func{
+class func:text{
     public:
+        int const systemNum = whichSystem();
+
         int clear(int systemNum);
         int press_any_key_to_continue();
+        void gameLoading();
+        void gameStart(text& gameText);
         float randomNumGenerator();
         int whichSystem();
         int displayWhichSystem(int whichSystem);
+
+        void sleeep(int systemNum);
 };
 
 class character{
@@ -32,7 +48,7 @@ class character{
         // 构造函数
         character(int hp, int attack, int defense):hp_(hp), attack_(attack), defence_(defense){}
 
-        int attack(character &enemy);
+        int attack(character &attackTarget);
         int getHP();
         int getDefence();
         bool isAlive();
@@ -40,6 +56,7 @@ class character{
         int decreaseDefForOneRound();
 
         int enemyRandomChoice(func& func);
+
     private:
     // 生命，攻击力，防御力属性
         int hp_;
@@ -47,11 +64,19 @@ class character{
         int defence_;
 };
 
+class battle{
+    public:
+        int battleField(character& player, character& enemy, func& func);
+
+        int playerRound(character& player, character& enemy, func& func);
+        int enemyRound(character& player, character& enemy);
+};
+
 
 
 // -------------------- text --------------------
 
-inline int text::printHello() {
+inline int const text::printHello() {
     cout << "\n\t欢迎来我的游戏！\n";
     return 0;
 }
@@ -62,6 +87,28 @@ inline int text::printHintForUnreadableCode(){
     return 0;
 }
 
+inline void text::alignCenterLinux(std::string text){
+    struct winsize w;
+    ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
+    int consoleWidth = w.ws_col;
+
+    // output
+    // std::string text = "Hello world";
+    int padding = (consoleWidth - text.length()) / 2 ;
+    cout << "\033[1A" << std::string(padding, ' ') << text << "\033[1B" << endl;
+}
+
+// inline void text::alignCenter(int systemNum){
+//     switch (systemNum) {
+//     case 2:
+        
+//         break;
+
+//     default:
+//         log("fault: no text align center!");
+//     }
+
+// }
 
 // -------------------- func --------------------
 
@@ -78,10 +125,35 @@ inline int func::clear(int systemNum){
 
 inline int func::press_any_key_to_continue()
 {
-    cout << "\n\n\t- 按任意键继续 -" << endl;
+    cout << "\n\n\t- 按回车键继续 -" << endl;
     getchar();
     return 0;
 }
+
+inline void func::gameLoading(){
+    // 啥呗进度条
+    clear(systemNum);
+    for(int i=0; i<1; i++)
+    {
+        clear(systemNum);
+        log("正在加载.");
+        sleep(1);
+        clear(systemNum);
+        log("正在加载..");
+        sleep(1);
+        clear(systemNum);
+        // log("正在加载...");
+        // sleep(1);
+    }
+    sleep(1);
+}
+
+inline void func::gameStart(text& gameText){
+    clear(systemNum);
+    gameText.printHello();
+    press_any_key_to_continue();
+}
+
 
 inline float func::randomNumGenerator(){
     std::mt19937 randomNumGenerator; // 创建一个随机数生成器
@@ -115,15 +187,25 @@ inline int func::displayWhichSystem(int whichSystem){
     }
 }
 
+inline void func::sleeep(int systemNum){
+    #if systemNum == 1 // windows
+        Sleep(500);
+    #elif systemNum == 2 // linux
+        sleep(1);
+    #else
+        log("CAN NOT RECOGNIZE YOUR SYSTEM");
+    #endif
+}
+
 
 // -------------------- character --------------------
 
-inline int character::attack(character &enemy){
-    int damage = attack_ - enemy.defence_;
+inline int character::attack(character &attackTarget){
+    int damage = attack_ - attackTarget.defence_;
     // 可正可负，要是护甲太高了就打不动
     // 造成的伤害是 你的攻击 - 敌人的护甲
     if(damage > 0)
-        enemy.hp_ -= damage;
+        attackTarget.hp_ -= damage;
     return damage;
 
 }
@@ -164,9 +246,61 @@ inline int character::enemyRandomChoice(func& func){
     return rNum;
 }
 
+// -------------------- battle --------------------
 
+inline int battle::battleField(character &player, character &enemy, func &func){
+    while (player.isAlive() && enemy.isAlive()) {
+        cout << "your hp is:" << player.getHP() << endl;
+        cout << "your enemy's hp is:" << enemy.getHP() << endl;
+        cout << "\nwhat's your choice!!!\n\t1.attack\t2.defence\n" << endl;
 
+        playerRound(player, enemy, func);
+        enemyRound(player, enemy);
+    }
+    return 0;
+}
 
+inline int battle::playerRound(character& player, character& enemy, func& func) {
+    int playerChoice;
+    cin >> playerChoice;
+
+    switch (playerChoice) {
+    case 1:
+        log("you are attacking your enemy!!");
+        // player.attack(enemy);
+        sleep(1);
+        cout << "你对敌方造成了" << player.attack(enemy) << "点伤害" << endl;
+        sleep(1);
+        func.clear(func.systemNum);
+        break;
+    case 2:
+        log("your enemy is going to attack you! DEFENCE!");
+        sleep(1);
+        player.increaseDefForOneRound();
+        cout << "你的防御力提高到了" << player.getDefence() + 5 << "点" << endl;
+        sleep(1);
+        func.clear(func.systemNum);
+        break;
+    default:
+        log("输数字，ok？");
+        sleep(1);
+        func.clear(func.systemNum);
+        break;
+    }
+    return 0;
+}
+
+inline int battle::enemyRound(character& player, character& enemy) {
+    // 如果敌人还在，就可以攻击玩家
+    if (enemy.isAlive()) {
+        int damage = enemy.attack(player);
+        log("敌人的回合！！！");
+        sleep(1);
+        cout << "敌方实际对你造成了" << damage << "点伤害!" << endl;
+        player.decreaseDefForOneRound();
+    }
+    return 0;
+}
 
 
 
